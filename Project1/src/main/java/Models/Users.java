1873ab85e4ce9b807dccf3f770be70ba87fb2e68
merchuey <mercedezhueyopa@gmail.com>
2022-05-18 21:42:16 -0700

    third push (user model) hope i did this right

--- Project1/src/main/java/Models/Users.java
+++ Project1/src/main/java/Models/Users.java
@@ -1,5 +1,34 @@
 package Models;
 
 public class Users {
+	private int id;
+	private String username;
+	private String password; 
+	private Roles role;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Roles getRole() {
+		return role;
+	}
+	public void setRole(Roles role) {
+		this.role = role;
+	}
+	
 
 }
