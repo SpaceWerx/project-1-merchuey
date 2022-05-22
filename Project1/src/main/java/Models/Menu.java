e58653c758ed8b622696894f58d16c10b9424e91
merchuey <mercedezhueyopa@gmail.com>
2022-05-21 20:39:09 -0700

    ninth upload *made things look neat and make sense

--- Project1/src/main/java/Models/Menu.java
+++ Project1/src/main/java/Models/Menu.java
@@ -12,36 +12,29 @@ public class Menu {
 		User_Servies es = new User_Servies(); //why do i have so many imports? is that okay?
 		Reimservices rs = new Reimservices();
 		
-		//All of the menu display options and control flow are contained within this method
 		public void displayMenu() throws SQLException {
 			
-			boolean displayMenu = true; //we're going to use this to toggle whether the menu continues after user input
-			Scanner scan = new Scanner(System.in); //Scanner object to parse (take) user input
+			boolean displayMenu = true; 
+			Scanner scan = new Scanner(System.in);
 			//what is the meaning of scan is never closed?
 			//give the user a pretty greeting :)
 			System.out.println("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*");
 			System.out.println("Welcome to Robin Hood Reimbursement Services");
 			System.out.println("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*");
 			
-			
-			//display the menu as long as the displayMenu boolean == true
-			//this is going to hold and display all my menu options until displayMenu == false
 			while(displayMenu) { 
 				
 				//menu options
 				System.out.println("hi! -> get greeted");
-				System.out.println("employees -> show all employees");
-				System.out.println("employeesById -> get employees with a certain ID");
-				System.out.println("employeesByTitle -> get employees of a certain title");
-				System.out.println("add -> add a new employee");
+				System.out.println("users -> show all users");
+				System.out.println("usersById -> get users with a certain ID");
+				System.out.println("usersByTitle -> get userss of a certain title");
+				System.out.println("add -> add a new users");
 				System.out.println("exit -> exit the application");
 				
-				
-				//parse user input after they choose a menu option, and put it into a String variable
 				String input = scan.nextLine();
 				
-				//switch statement that takes the user input and executes the appropriate code
-				//BEN MAY ADD MORE COMMENTS HERE ONCE WE DO DATABASE CONNECTIVITY
+				
 				switch(input) {
 				
 				case "hi": {
@@ -54,7 +47,7 @@ public class Menu {
 					break; //we need a break in each case block, or else all the other cases will still run
 				}
 				
-				case "employees" :{
+				case "users" :{
 					
 					//get the List of employees from the repository layer
 					List<Users> users = es.users();
@@ -119,15 +112,11 @@ public class Menu {
 					String l_name = scan.nextLine();
 					
 					System.out.println("Enter Role Id: 1) Manager 2) Accountatnt 3)  4) Marketing 5) Director");
-					int roleId = scan.nextInt(); //we need nextInt because ID is an int datatype in the database
-					scan.nextLine(); //without any nextLine(), your enter keystroke will be grabbed as the next input
-					//so we need nextLine() in order to actually move to the..... NEXT line!
+					int roleId = scan.nextInt(); 
+					scan.nextLine(); 
 					
-					//Given all this information, we'll create a new Employee object to send to the service layer
-					//then the service layer will send it to the repository layer.
 					Users newUsers = new Users (f_name, l_name, roleId);
 					
-					//Put the new Employee into the addEmployee() method in the EmployeeService Class
 					es.addUsers(newUsers);
 					
 					break;
