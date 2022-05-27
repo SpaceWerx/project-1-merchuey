a950c74cdd540f3e3814fac03076bd1e1f35315e
merchuey <mercedezhueyopa@gmail.com>
2022-05-26 17:28:31 -0700

    my dignityyyy

--- Project1/src/main/java/Utilities/ConnectionFactoryUtility.java
+++ Project1/src/main/java/Utilities/ConnectionFactoryUtility.java
@@ -52,7 +52,7 @@ public class ConnectionFactoryUtility {
         //we'll hardcode them for now, but I'll show a way to hide the credentials in environment variables
 
         //the url to my database schema
-        String url = "jdbc:postgresql://project-one.cmzf3imydhe1.us-east-2.rds.amazonaws.com:5432/postgres?currentSchema=project1"; 
+        String url = "jdbc:postgresql://javafullstackaws.cnnbe3eybd7e.us-west-1.rds.amazonaws.com:5432/postgres?currentSchema=p1"; 
         //your postgres username (should just be postgres)
         String username = "postgres";
         //your postgres password (hopefully just "password")
--- Project1/src/main/java/com/revature/Launcher.java
+++ Project1/src/main/java/com/revature/Launcher.java
@@ -2,6 +2,7 @@ package com.revature;
 
 import java.sql.Connection;
 
+
 import java.sql.SQLException;
 
 import Controller.AuthController;
